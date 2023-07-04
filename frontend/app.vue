e43a2bfa7ba78cae00315080b71278433ed49ca8
Tho Vu <robert_vu947@protonmail.com>
2023-07-04 16:56:35 +0800

    add navbar

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="min-h-screen flex flex-col bg-sushi-50">
+  <div class="min-h-screen flex flex-col bg-sushi-50 pt-24">
+    <Navbar />
     <NuxtPage />
   </div>
 </template>
